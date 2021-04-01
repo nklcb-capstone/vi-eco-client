58980d19d666fc859238a543db8a9285795241b9
0jun <kayo10ve@kyonggi.ac.kr>
2021-04-01 13:42:59 +0900

    20210401

--- src/components/kakaoAPI/Map.tsx
+++ src/components/kakaoAPI/Map.tsx
@@ -44,7 +44,7 @@ const Map: React.FC = () => {
 
   return (
     <div className="Map">
-      <div id="map" style={{ width: '100vw', height: '100vh' }} />
+      <div id="map" style={{ width: '100vw', height: '95vh' }} />
     </div>
   );
 };
