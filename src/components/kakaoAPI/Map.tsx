e781f5e2ea5ee03a9f47c2a5531ef69030cc32c5
minsu-zip <alstn4814@gmail.com>
2021-04-04 02:54:34 +0900

    [ADD] 인포윈도우 이벤트 작성

--- src/components/kakaoAPI/Map.tsx
+++ src/components/kakaoAPI/Map.tsx
@@ -12,22 +12,43 @@ const Map: React.FC = () => {
     mapscript();
   }, []);
 
+  // 인포윈도우를 표시하는 클로저를 만드는 함수입니다
+  const makeOverListener: any = (map: any, marker: any, infowindow: any) => {
+    return function () {
+      infowindow.open(map, marker);
+    };
+  };
+
+  // 인포윈도우를 닫는 클로저를 만드는 함수입니다
+  const makeOutListener: any = (infowindow: any) => {
+    return function () {
+      infowindow.close();
+    };
+  };
+
   const mapscript = () => {
     // kaka API 띄우기
     let container = document.getElementById('map');
     let options = {
-      //위도 경도 서울역 위치
+      //지도 기본 위치값 서울역 좌표로 지정
       center: new window.kakao.maps.LatLng(37.555178, 126.970756),
       level: 5,
     };
     let map: any = new window.kakao.maps.Map(container, options);
 
-    // 마커 한개 생성 및 띄우기
+    // 마커 한개 생성 및 띄우기 서울역
     let markerPosition = new kakao.maps.LatLng(37.555163, 126.970768);
-
     let marker = new kakao.maps.Marker({
+      map: map,
       position: markerPosition,
     });
+    //인포윈도우 띄우기
+    let infowindow = new kakao.maps.InfoWindow({
+      content: '서울역',
+    });
+    //윈포윈도우에 이벤트 적용하기
+    kakao.maps.event.addListener(marker, 'mouseover', makeOverListener(map, marker, infowindow));
+    kakao.maps.event.addListener(marker, 'mouseout', makeOutListener(infowindow));
 
     //markerDate에 있는 마커 여러개 생성 및 표시
     markerdata.map((el) => {
@@ -37,9 +58,6 @@ const Map: React.FC = () => {
         title: el.title,
       });
     });
-
-    //마커를 지도 위에 표시
-    marker.setMap(map);
   };
 
   return (
