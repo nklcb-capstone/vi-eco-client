3de262eab18e43091307d501f90543d2991af77d
minsu-zip <alstn4814@gmail.com>
2021-04-08 10:03:27 +0900

    [REFACTOR]

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,34 @@
 import React from 'react';
+import logo from './logo.png';
 import './App.scss';
 import Test from 'components/Test';
 import Map from 'components/kakaoAPI/Map';
-import Layout from 'components/viecoLayout/viecoLayout';
 
 function App() {
   return (
     <div className="App">
-      <header></header>
-      <body>
-        <Map></Map>
-      </body>
-      <footer>
-        <Layout></Layout>
-      </footer>
+      {/* <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a className="App-link" href="https://github.com/nklcb-capstone" target="_blank" rel="noopener noreferrer">
+          Github Repository
+        </a>
+        <a
+          className="App-link"
+          href="https://www.notgition.so/e4b2f84dd0124772af02a8716350b218"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Notion
+        </a>
+      </header>
+      <Test /> */}
+
+      {/* 지도 API 컴포넌트 */}
+
+      <Map></Map>
     </div>
   );
 }
---
+++ src/common/api/datagokrApi.tsx
@@ -0,0 +1,26 @@
+import axios from 'axios';
+import React from 'react';
+
+// const datagokrApi: React.FC = () => {
+//   axios
+//     .get(
+//       'http://ec2-3-36-231-201.ap-northeast-2.compute.amazonaws.com:8080/api/car/electric/station?numOfRows=3&pageNo=1',
+//     )
+//     .then(function (response) {
+//       console.log(response);
+//       // response
+//     })
+//     .catch(function (error) {
+//       // 오류발생시 실행
+//     })
+//     .then(function () {
+//       // 항상 실행
+//     });
+
+//   return (
+//     <>
+//       <div>as</div>
+//     </>
+//   );
+// };
+// export default datagokrApi;
