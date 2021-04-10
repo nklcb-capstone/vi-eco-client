6da311033a368732564ca57d0e08fd1d38c9b0c6
0jun <kayo10ve@kyonggi.ac.kr>
2021-04-10 19:43:52 +0900

    [FIX] React Hook Tutorial

--- src/components/viecoLayout/viecoLayout.tsx
+++ src/components/viecoLayout/viecoLayout.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { Layout } from 'antd';
 import { CarOutlined } from '@ant-design/icons';
@@ -7,34 +6,35 @@ import './viecoLayout.scss';
 const { Header, Content, Footer } = Layout;
 
 function Menubar() {
-  const [pageMode, switchMode] = useState(['전기차 충전소 위치', '수소차 충전소 위치']);
+  const [pageMode, setPageMode] = useState('전기차 충전소 위치');
 
-  function changeMod(){
-    const newArray = [...pageMode];
-    if (newArray[0]=='전기차 충전소 위치'){
-      newArray[0] = '수소차 충전소 위치';
+  //React Hook Tutorial
+  function changeMod() {
+    if (pageMode === '전기차 충전소 위치') {
+      setPageMode('수소차 충전소 위치');
     } else {
-      newArray[0] = '전기차 충전소 위치';
+      setPageMode('전기차 충전소 위치');
     }
-    switchMode( newArray );
   }
+
   return (
-      <div className = "menubar">
-       <span> { pageMode[0] } </span>
-       <span>주변 정보</span>
-       <span>관련 정보</span>
-       <span id="modchange" onClick={ changeMod }>모드 전환</span>
-      </div>
+    <div className="menubar">
+      <span> {pageMode} </span>
+      <span>주변 정보</span>
+      <span>관련 정보</span>
+      <span id="modchange" onClick={changeMod}>
+        모드 전환
+      </span>
+    </div>
   );
 }
 
 function viecoLayout() {
-  return(
-  <div className="viecoLayout"> 
-    <Menubar></Menubar>
-  </div>
+  return (
+    <div className="viecoLayout">
+      <Menubar></Menubar>
+    </div>
   );
 }
-
 
 export default viecoLayout;
