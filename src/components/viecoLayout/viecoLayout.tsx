fd70822e8dc51aa2af686645d1d986c5ee33c227
minsu-zip <alstn4814@gmail.com>
2021-04-18 02:15:17 +0900

    [REFACTOR] Mode 전환 구조 변경

--- src/components/viecoLayout/viecoLayout.tsx
+++ src/components/viecoLayout/viecoLayout.tsx
@@ -1,4 +1,3 @@
-
 import React, { useState } from 'react';
 import { Layout } from 'antd';
 import { CarOutlined } from '@ant-design/icons';
@@ -6,35 +5,27 @@ import './viecoLayout.scss';
 
 const { Header, Content, Footer } = Layout;
 
-function Menubar() {
-  const [pageMode, switchMode] = useState(['전기차 충전소 위치', '수소차 충전소 위치']);
+const ViecoLayout: React.FC = () => {
+  const [pageMode, SetpageMode]: any = useState(true);
+  const [pageName, SetpageName]: any = useState('전기차 충전소 위치');
+  const changeMod = () => {
+    SetpageMode(!pageMode);
 
-  function changeMod(){
-    const newArray = [...pageMode];
-    if (newArray[0]=='전기차 충전소 위치'){
-      newArray[0] = '수소차 충전소 위치';
-    } else {
-      newArray[0] = '전기차 충전소 위치';
-    }
-    switchMode( newArray );
-  }
+    SetpageName(pageMode ? '전기차 충전소 위치' : '수소차 충전소 위치');
+  };
+
   return (
-      <div className = "menubar">
-       <span> { pageMode[0] } </span>
-       <span>주변 정보</span>
-       <span>관련 정보</span>
-       <span id="modchange" onClick={ changeMod }>모드 전환</span>
+    <div className="viecoLayout">
+      <div className="menubar">
+        <span> {pageName} </span>
+        <span>주변 정보</span>
+        <span>관련 정보</span>
+        <span id="modchange" onClick={changeMod}>
+          모드 전환
+        </span>
       </div>
+    </div>
   );
-}
+};
 
-function viecoLayout() {
-  return(
-  <div className="viecoLayout"> 
-    <Menubar></Menubar>
-  </div>
-  );
-}
-
-
-export default viecoLayout;
+export default ViecoLayout;
