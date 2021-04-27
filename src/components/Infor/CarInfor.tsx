8ceba2015a05b316749d6e88ae513adbc23c86da
0jun <kayo10ve@kyonggi.ac.kr>
2021-04-27 13:23:43 +0900

    [FIX] 그리드 주석

--- src/components/Infor/CarInfor.tsx
+++ src/components/Infor/CarInfor.tsx
@@ -10,24 +10,28 @@ const { Meta } = Card;
 
 function CarInfo() {
   const cols = [];
-  //i = 화면에 표시되는 카드 수
-  for (let i = 0; i < 3; i++) {
-    cols.push(
-      <Col key={i.toString()} span={24 / 3}>
-        <Card
-          hoverable
-          style={{ width: 400, height: 380 }}
-          cover={
-            <img
-              alt="example"
-              src="https://imgauto-phinf.pstatic.net/20210223_189/auto_1614065502912dSObk_PNG/20210223163129_ipfD3sWg.png?type=f567_410"
-            />
-          }
-        >
-          <Meta title="2021 아이오닉 5" description="출시가 4,980~5,755 만원" />
-        </Card>
-      </Col>,
-    );
+  // 행열 가이드      3 3 3
+  // row : 2 (행)  2 ㅁㅁㅁ
+  // col : 3 (열)  2 ㅁㅁㅁ
+  for (let row = 0; row < 2; row++) {
+    for (let col = 0; col < 3; col++) {
+      cols.push(
+        <Col key={col.toString()} span={24 / 3}>
+          <Card
+            hoverable
+            style={{ width: 400, height: 380 }}
+            cover={
+              <img
+                alt="example"
+                src="https://imgauto-phinf.pstatic.net/20210223_189/auto_1614065502912dSObk_PNG/20210223163129_ipfD3sWg.png?type=f567_410"
+              />
+            }
+          >
+            <Meta title="2021 아이오닉 5" description="출시가 4,980~5,755 만원" />
+          </Card>
+        </Col>,
+      );
+    }
   }
 
   return (
@@ -44,7 +48,6 @@ function CarInfo() {
         <Divider orientation="left">전기차</Divider>
         <MediaQuery maxWidth={1920}>
           <Row gutter={[16, 24]}>
-            {cols}
             {cols}
           </Row>
         </MediaQuery>
