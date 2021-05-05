d24c362ba1d25a5a7a6efb93c6e141359733bb77
minsu-zip <alstn4814@gmail.com>
2021-05-06 00:53:30 +0900

    [ADD] 차량 모델 검색기능 추가

--- src/components/Infor/CarInfo.tsx
+++ src/components/Infor/CarInfo.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import MediaQuery from 'react-responsive';
 import axios from 'axios';
 import { Layout, Menu } from 'antd';
+import { Space, Input, List } from 'antd';
 import { Row, Col, Divider } from 'antd';
 import { Card } from 'antd';
 import { Pagination } from 'antd';
@@ -9,6 +10,7 @@ import { Link } from 'react-router-dom';
 
 const { Header, Content, Footer } = Layout;
 const { Meta } = Card;
+const { Search } = Input;
 
 const CarInfo = () => {
   const cols = [];
@@ -16,16 +18,21 @@ const CarInfo = () => {
   // row : 2 (행)  2 ㅁㅁㅁ
   // col : 3 (열)  2 ㅁㅁㅁ
   const [carList, setCarList] = useState([]);
+  const [search, setSearch] = useState<any>('');
 
   const getDate = async () => {
-    const { data } = await axios.get('https://vi-eco.jseung.me/api/car/information/search');
+    const { data } = await axios.get(`https://vi-eco.jseung.me/api/car/information/search?carName=${search}`);
     setCarList(data);
-    console.log(carList[1]);
+  };
+
+  const onSearch = (value: any): void => {
+    console.log(value);
+    setSearch(value);
   };
 
   useEffect(() => {
     getDate();
-  }, []);
+  }, [search]);
 
   return (
     <Layout className="layout">
@@ -48,7 +55,14 @@ const CarInfo = () => {
       </Header>
       <Content style={{ padding: '0 50px' }}>
         <Divider orientation="left">전기차</Divider>
-
+        <Search
+          style={{ paddingBottom: '20px' }}
+          placeholder="input search text"
+          allowClear
+          enterButton="Search"
+          size="large"
+          onSearch={onSearch}
+        />
         <Row gutter={[24, 24]}>
           {carList.map((el: any) => (
             <Col key={el.id} className="gutter-row" xs={24} md={12} lg={6}>
